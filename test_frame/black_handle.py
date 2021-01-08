9ec9201580a1ee687087c187ee28bf15b7d7b329
zhangyukun <e1281480917eqqcom>
2021-01-08 13:59:25 +0800

    打造自己的测试框架 （一）

---
+++ test_frame/black_handle.py
@@ -0,0 +1,34 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
+def black_a(fun):
+    def run(*args,**kwargs):
+        basepage=args[0]
+        try:
+            return fun(*args,**kwargs)
+        except NoSuchElementException as e:
+            for black in basepage.black_list:
+                value = basepage.driver.find_element(*black)
+                if value != None:
+                    value.click()
+                    return fun(*args,**kwargs)
+            raise e
+    return run
+def black_wrapper(fun):
+    def run(*args, **kwargs):
+        basepage = args[0]
+        try:
+            return fun(*args, **kwargs)
+        # 捕获元素没找到异常
+        except Exception as e:
+            # 遍历黑名单中的元素，进行处理
+            for black in basepage.black_list:
+                eles = basepage.finds(*black)
+                # 黑名单被找到
+                if len(eles) > 0:
+                    # 对黑名单元素进行点击，可以自由扩展
+                    eles[0].click()
+                    return fun(*args, **kwargs)
+            raise e
+
+    return run
