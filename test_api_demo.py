f4c1e582b5f0dd7592d1f21983ffa1eda6404b3e
zhangyukun <e1281480917eqqcom>
2021-01-29 10:06:33 +0800

    企业微信接口自动化实战【一】

---
+++ test_api_demo.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+
+def test_get_access_token():
+    url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=ww15439cad488e610c&corpsecret=Bly_4jl9M9STdZ66VqYe19wKmkKim9Tdlux76EQeo-s'
+    token_response = requests.get(url)
+    token = json.loads(token_response.text)
+    print(token)
+    return token['access_token']
+
+def test_add_member():
+    url = f'https://qyapi.weixin.qq.com/cgi-bin/user/create?access_token={test_get_access_token()}'
+    data = {
+        "userid": "TomBlank",
+        "name": "TomBlank",
+        "department": "1",
+        "mobile": "13567676666",
+    }
+    add_member_response = requests.post(url=url, json=data)
+    add_member = json.loads(add_member_response.text)
+    print(add_member)
+    assert 'created' in add_member['errmsg']
+
+def test_delete_member():
+    url = f'https://qyapi.weixin.qq.com/cgi-bin/user/delete?access_token={test_get_access_token()}&userid=TomBlank'
+    delete_member_response = requests.get(url)
+    delete_member = json.loads(delete_member_response.text)
+    assert delete_member['errmsg'] == 'deleted'
